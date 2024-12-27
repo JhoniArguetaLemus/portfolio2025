f28d925f3f67a64dc9ec9bc9bbd821f55cf54039
JhoniArguetaLemus <isnp111022@ugb.edu.sv>
2024-12-27 09:24:52 -0600

    first commit

--- js/app.js
+++ js/app.js
@@ -24,7 +24,27 @@ btMenu.addEventListener('click', ()=>{
 
 
 
+const carousel = document.getElementById('carousel');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
 
+const totalSlides = carousel.children.length;
+let currentIndex = 0;
+
+function updateCarousel() {
+  const offset = -currentIndex * 100;
+  carousel.style.transform = `translateX(${offset}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  updateCarousel();
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  updateCarousel();
+});
 
 
 
